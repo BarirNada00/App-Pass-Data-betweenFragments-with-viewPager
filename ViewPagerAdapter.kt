a2dba4de109464e0e4f149ccf052c2bc95b02755
BarirNada00 <116973510+BarirNada00@users.noreply.github.com>
2023-02-01 20:06:32 +0100

    Add files via upload

---
+++ ViewPagerAdapter.kt
@@ -0,0 +1,29 @@
+package ma.projet.android.frag
+
+import android.content.res.Resources
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount() = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                FragmentA()
+            }
+            1 -> {
+                FragmentB()
+            }
+            else -> {
+                throw Resources.NotFoundException("Position Not Found")
+            }
+        }
+
+
+    }
+
+
+}
